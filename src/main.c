95b983fba86d8e0dad219d53bcc31fc468a6ea1b
Davorin Šego <davorin.sego@gmail.com>
2013-04-13 12:55:25 +0200

    temporary main function to test the engine

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <time.h>
+#include "engine.h"
+
+
+int main(int argc, char *argv[])
+{
+  Engine* engine = Engine_create();
+  Engine_init_audio(engine);
+  Note note;
+  note.frequency = 440;
+
+  while (true) {
+    Engine_read_strobes(engine, note);
+    Engine_estimate_pitch(engine);
+    nanosleep((struct timespec[]){{0, 200000000}}, NULL);
+  }
+  return 0;
+}
