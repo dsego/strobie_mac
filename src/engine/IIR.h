e7d92da06e7fe0fe0e7e057f6aaff78b1e5af8d8
Davorin Šego <davorin.sego@gmail.com>
2013-05-02 23:18:39 +0200

    IIR implementation

---
+++ src/engine/IIR.h
@@ -0,0 +1,35 @@
+/*
+    Copyright (c) 2013 Davorin Šego. All rights reserved.
+ */
+
+
+#define MAX_ARRAY_SIZE 10
+
+
+typedef struct {
+
+  int numSecondOrderStages;
+  int firstOrderStage;
+
+  double w[MAX_ARRAY_SIZE][3];
+  double a[MAX_ARRAY_SIZE][3];
+  double b[MAX_ARRAY_SIZE][3];
+
+} IIR;
+
+
+IIR* IIR_CreateHalfbandEllip();
+
+IIR* IIR_CreateHalfbandCheby();
+
+IIR* IIR_CreateQuartbandEllip();
+
+IIR* IIR_CreateQuartbandCheby();
+
+IIR* IIR_CreateThreePercEllip();
+
+IIR* IIR_CreateThreePercCheby();
+
+void IIR_filter(IIR* self, float* input, float* output, int length);
+
+void IIR_reset(IIR* self);
