19cc45b2a7aa18c2b50f35172918fd9189085c09
Davorin Šego <davorin.sego@gmail.com>
2013-09-17 03:22:34 +0200

    remove junk

--- src/mac/StrobeView.m
+++ src/mac/StrobeView.m
@@ -144,32 +144,6 @@ typedef struct {
 
   NSOpenGLContext* context = [self openGLContext];
   [context makeCurrentContext];
-//   // CGLContextObj cglContext = [context CGLContextObj];
-//  // printf("adsf\n");
-// // must lock GL context because display link is threaded
-//   // CGLLockContext(cglContext);
-
-
-//   glClearColor(1.0, 0.0, 0.0, 1);
-//   glClear(GL_COLOR_BUFFER_BIT);
-//     glColor3f(1.0f, 0.85f, 0.35f);
-//     glBegin(GL_TRIANGLES);
-//     {
-//         glVertex3f(  0.0,  0.6, 0.0);
-//         glVertex3f( -0.2, -0.3, 0.0);
-//         glVertex3f(  0.2, -0.3 ,0.0);
-//     }
-//     glEnd();
-
-// // printf("drawFrame\n");
-
-// // glFlush();
-// [context flushBuffer];
-
-// // CGLFlushDrawable(cglContext);
-// // CGLUnlockContext(cglContext);
-
-// return;
 
   glClearColor(0.0, 0.0, 0.0, 1);
   glClear(GL_COLOR_BUFFER_BIT);
