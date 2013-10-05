4a31dfbb454a1de230d45e1dfc4ef74d9c54534b
Davorin Šego <davorin.sego@gmail.com>
2013-10-06 00:08:23 +0200

    update config

--- src/engine/config.c
+++ src/engine/config.c
@@ -18,29 +18,29 @@ Config* Config_create() {
   self->outputDevice = 0;
   self->samplerate   = 44100;
 
-  self->fftLength      = 2048;
+  self->fftLength      = 4096;
   self->pitchStandard  = 440;
   self->displayFlats   = 0;
   self->centsOffset    = 0;
   self->transpose      = 0;
   self->audioThreshold = -60;
 
-  self->reference = Tuning12TET_find(110, self->pitchStandard, self->centsOffset, self->transpose);
+  self->reference = Tuning12TET_find(110, self->pitchStandard, self->centsOffset);
 
   int samplesPerPeriod = 1024;
-  float periodsPerFrame = 0.5;
+  float periodsPerFrame = 1;
 
-  self->strobeCount = 8;
+  self->strobeCount = 5;
 
   for (int i = 0; i < self->strobeCount; ++i) {
 
-    self->strobes[i].color1[0] = 253;
-    self->strobes[i].color1[1] = 151;
-    self->strobes[i].color1[2] = 32;
+    self->strobes[i].color1[0] = 217;
+    self->strobes[i].color1[1] = 255;
+    self->strobes[i].color1[2] = 163;
 
-    self->strobes[i].color2[0] = 43;
-    self->strobes[i].color2[1] = 33;
-    self->strobes[i].color2[2] = 16;
+    self->strobes[i].color2[0] = 28;
+    self->strobes[i].color2[1] = 80;
+    self->strobes[i].color2[2] = 161;
 
     self->strobes[i].samplesPerPeriod = samplesPerPeriod;
     self->strobes[i].periodsPerFrame = periodsPerFrame;
@@ -49,7 +49,7 @@ Config* Config_create() {
     self->strobes[i].centsOffset = 0;
     self->strobes[i].gain = 1000;
     self->strobes[i].mode = OCTAVE;
-    self->strobes[i].value = i;
+    self->strobes[i].value = i + 1;
     self->strobes[i].subdivCount = 0;
 
     samplesPerPeriod /= 2;
