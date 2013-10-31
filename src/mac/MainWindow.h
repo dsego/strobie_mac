816801e0b65f9511cbb4e9db8da411d3058f1538
Davorin Šego <davorin.sego@gmail.com>
2013-10-31 18:16:40 +0100

    enable moving the window by dragging anywhere, not just the title bar

---
+++ src/mac/MainWindow.h
@@ -0,0 +1,9 @@
+/*
+    Copyright (c) 2013 Davorin Šego. All rights reserved.
+*/
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainWindow : NSWindow
+
+@end
---
+++ src/mac/MainWindow.m
@@ -0,0 +1,19 @@
+/*
+    Copyright (c) 2013 Davorin Šego. All rights reserved.
+*/
+
+#import "MainWindow.h"
+
+
+
+@implementation MainWindow
+
+
+- (BOOL)isMovableByWindowBackground {
+
+  return YES;
+
+}
+
+
+@end
