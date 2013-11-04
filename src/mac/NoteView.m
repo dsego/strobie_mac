af6b34217b43c6eb2452319500d99e2ee75dcb42
Davorin Šego <davorin.sego@gmail.com>
2013-11-04 23:01:30 +0100

    note view and selection menu

--- src/mac/NoteView.m
+++ src/mac/NoteView.m
@@ -9,9 +9,9 @@
 
 @implementation NoteView {
 
-  NSTextStorage *textStorage;
-  NSLayoutManager *layoutManager;
-  NSTextContainer *textContainer;
+  CFAttributedStringRef noteNames[12];
+  CFAttributedStringRef sharpSign;
+  NSMenu *noteMenu;
 
 }
 
@@ -19,27 +19,172 @@
 - (id)initWithFrame:(NSRect)frameRect {
 
   self = [super initWithFrame: frameRect];
-  textStorage = [[NSTextStorage alloc] initWithString:@"      "];
-  [textStorage setFont: [NSFont fontWithName:@"Menlo" size: 72]];
-
-  layoutManager = [[NSLayoutManager alloc] init];
-  textContainer = [[NSTextContainer alloc] init];
-  [layoutManager addTextContainer:textContainer];
-  [textStorage addLayoutManager:layoutManager];
-
+  [self buildStrings];
+  [self buildMenu];
   return self;
 
 }
 
 
-- (void)drawRect: (NSRect)rect {
+- (void)buildStrings {
 
-  NSString *str = [NSString stringWithFormat: @"%c%u%C", engine->currentNote.letter, engine->currentNote.octave, engine->currentNote.accidental];
-  [textStorage replaceCharactersInRange: NSMakeRange(0, [str length]) withString: str];
-  NSRange glyphRange = [layoutManager glyphRangeForTextContainer: textContainer];
-  [layoutManager drawGlyphsForGlyphRange: glyphRange atPoint: (NSPoint) {0, -52}];
+  CFStringRef keys1[] = {
+    kCTFontAttributeName,
+    kCTForegroundColorAttributeName
+  };
+
+  CFTypeRef values1[] = {
+    CTFontCreateWithName(CFSTR("HelveticaNeue"), 72, NULL),
+    CGColorCreateGenericRGB(0.95, 0.95, 0.95, 1),
+  };
+
+  CFDictionaryRef attrs1 = CFDictionaryCreate(
+    kCFAllocatorDefault,
+    (const void**)&keys1,
+    (const void**)&values1,
+    sizeof(keys1) / sizeof(keys1[0]),
+    &kCFTypeDictionaryKeyCallBacks,
+    &kCFTypeDictionaryValueCallBacks
+  );
+
+  noteNames[0] = CFAttributedStringCreate(kCFAllocatorDefault, CFSTR("C"), attrs1);
+  noteNames[1] = CFAttributedStringCreate(kCFAllocatorDefault, CFSTR("C"), attrs1);
+  noteNames[2] = CFAttributedStringCreate(kCFAllocatorDefault, CFSTR("D"), attrs1);
+  noteNames[3] = CFAttributedStringCreate(kCFAllocatorDefault, CFSTR("D"), attrs1);
+  noteNames[4] = CFAttributedStringCreate(kCFAllocatorDefault, CFSTR("E"), attrs1);
+  noteNames[5] = CFAttributedStringCreate(kCFAllocatorDefault, CFSTR("F"), attrs1);
+  noteNames[6] = CFAttributedStringCreate(kCFAllocatorDefault, CFSTR("F"), attrs1);
+  noteNames[7] = CFAttributedStringCreate(kCFAllocatorDefault, CFSTR("G"), attrs1);
+  noteNames[8] = CFAttributedStringCreate(kCFAllocatorDefault, CFSTR("G"), attrs1);
+  noteNames[9] = CFAttributedStringCreate(kCFAllocatorDefault, CFSTR("A"), attrs1);
+  noteNames[10] = CFAttributedStringCreate(kCFAllocatorDefault, CFSTR("A"), attrs1);
+  noteNames[11] = CFAttributedStringCreate(kCFAllocatorDefault, CFSTR("B"), attrs1);
+
+
+
+  CFStringRef keys2[] = {
+    kCTFontAttributeName,
+    kCTForegroundColorAttributeName
+  };
+
+  CFTypeRef values2[] = {
+    CTFontCreateWithName(CFSTR("HelveticaNeue"), 48, NULL),
+    CGColorCreateGenericRGB(0.95, 0.95, 0.95, 1),
+  };
+
+  CFDictionaryRef attrs2 = CFDictionaryCreate(
+    kCFAllocatorDefault,
+    (const void**)&keys2,
+    (const void**)&values2,
+    sizeof(keys2) / sizeof(keys2[0]),
+    &kCFTypeDictionaryKeyCallBacks,
+    &kCFTypeDictionaryValueCallBacks
+  );
+
+  sharpSign = CFAttributedStringCreate(kCFAllocatorDefault, CFSTR("♯"), attrs2);
 
 }
+
+
+- (void)buildMenu {
+
+  noteMenu = [[NSMenu alloc] initWithTitle:@"Note Menu"];
+  [noteMenu addItemWithTitle:@"C" action:@selector(menuAction:) keyEquivalent:@""];
+  [noteMenu addItemWithTitle:@"C♯" action:@selector(menuAction:) keyEquivalent:@""];
+  [noteMenu addItemWithTitle:@"D" action:@selector(menuAction:) keyEquivalent:@""];
+  [noteMenu addItemWithTitle:@"D♯" action:@selector(menuAction:) keyEquivalent:@""];
+  [noteMenu addItemWithTitle:@"E" action:@selector(menuAction:) keyEquivalent:@""];
+  [noteMenu addItemWithTitle:@"F" action:@selector(menuAction:) keyEquivalent:@""];
+  [noteMenu addItemWithTitle:@"F♯" action:@selector(menuAction:) keyEquivalent:@""];
+  [noteMenu addItemWithTitle:@"G" action:@selector(menuAction:) keyEquivalent:@""];
+  [noteMenu addItemWithTitle:@"G♯" action:@selector(menuAction:) keyEquivalent:@""];
+  [noteMenu addItemWithTitle:@"A" action:@selector(menuAction:) keyEquivalent:@""];
+  [noteMenu addItemWithTitle:@"A♯" action:@selector(menuAction:) keyEquivalent:@""];
+  [noteMenu addItemWithTitle:@"B" action:@selector(menuAction:) keyEquivalent:@""];
+
+
+  int tag = 0;
+
+  for (NSMenuItem *item in [noteMenu itemArray]) {
+    item.target = self;
+    item.tag = tag;
+    ++tag;
+  }
+
+  [noteMenu setAutoenablesItems:NO];
+  [noteMenu setMinimumWidth:100];
+
+}
+
+
+-(void)menuAction:(id)sender  {
+
+  int noteIndex = [sender tag];
+  Note note = Tuning12TET_noteFromIndex(noteIndex, 0, engine->config->pitchStandard, engine->config->centsOffset);
+  Engine_setStrobes(engine, note);
+  [self setNeedsDisplay: YES];
+
+}
+
+
+- (BOOL)mouseDownCanMoveWindow {
+
+  return NO;
+
+}
+
+
+- (void)drawRect:(NSRect)rect {
+
+  CGContextRef context = (CGContextRef) [[NSGraphicsContext currentContext] graphicsPort];
+
+  Note note;
+
+  if (engine->config->transpose == 0) {
+    note = engine->currentNote;
+  }
+  else {
+    note = Tuning12TET_transpose(engine->currentNote, engine->config->transpose);
+  }
+
+  // note name
+  CTLineRef line = CTLineCreateWithAttributedString(noteNames[note.index]);
+  CGRect bounds = CTLineGetImageBounds(line, context);
+
+  // center horizontally
+  float x = (rect.size.width - bounds.size.width) * 0.5;
+
+  CGContextSetTextPosition(context, x, 14);
+  CTLineDraw(line, context);
+  CFRelease(line);
+
+  // draw sharp symbol
+  if (note.isSharp) {
+    line = CTLineCreateWithAttributedString(sharpSign);
+    CGContextSetTextPosition(context, x + bounds.origin.x + bounds.size.width, 36);
+    CTLineDraw(line, context);
+    CFRelease(line);
+  }
+
+}
+
+
+- (void)mouseDown:(NSEvent *)theEvent {
+
+  if (engine->mode == MANUAL) {
+
+    NSPoint location = [self convertPoint:[theEvent locationInWindow] fromView:nil];
+
+    // TODO change highlighted to currently selected
+    [noteMenu popUpMenuPositioningItem: [noteMenu itemWithTag: engine->currentNote.index]
+      atLocation:NSMakePoint(0, location.y + 10) // dirty hack
+      inView:self
+    ];
+
+  }
+
+}
+
 
 
 @end
