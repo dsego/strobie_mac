0ce31d677d48eda2d50122782cec40367e157b9b
Davorin Šego <davorin.sego@gmail.com>
2013-12-02 21:35:54 +0100

    audio feed expermiental code

--- src/engine/AudioFeed.c
+++ src/engine/AudioFeed.c
@@ -57,11 +57,11 @@ void AudioFeed_read(AudioFeed* self, float* output, int length) {
 // 150Hz high pass Butterworth filter
 inline static void highpass(float *input, float* output, int length) {
 
-  const float a0 = 0.9850016172570234;
+  const float a0 =  0.9850016172570234;
   const float a1 = -1.9700032345140468;
-  const float a2 = 0.9850016172570234;
+  const float a2 =  0.9850016172570234;
   const float b1 = -1.9697782746275025;
-  const float b2 = 0.9702281944005912;
+  const float b2 =  0.9702281944005912;
 
   static float z1 = 0.0;
   static float z2 = 0.0;
@@ -78,13 +78,55 @@ inline static void highpass(float *input, float* output, int length) {
 }
 
 
+// moving average
+// void smooth(float *input, float *output, int length) {
+
+//   const int n = 16;
+//   const float k = 1.0 / (float)n;
+//   static float samples[n] = { 0.0 };
+//   static float mean = 0.0;
+
+//   for (int i = 0; i < n; ++i) {
+//     mean = mean + (input[i] - samples[i]) * k;
+//     output[i] = mean;
+//   }
+
+//   for (int i = n; i < length; ++i) {
+//     mean = mean + (input[i] - input[i -n]) * k;
+//     output[i] = mean;
+//   }
+
+//   for (int i = length - n, j=0; i < length; ++i, ++j) {
+//     samples[j] = input[i];
+//   }
+
+// }
+
+
 void AudioFeed_process(AudioFeed* self, float* input, int length) {
 
-  // attenuate below 150Hz
-  // highpass(input, self->filteredData, length);
+  // const int n = 1024;
+  // static float smoothed[n];
+  // int len = length;
+  // int index = 0;
+
+  // while (len > n) {
+  //   smooth(&input[index], smoothed, n);
+  //   PaUtil_WriteRingBuffer(self->ringbuffer, smoothed, n);
+  //   len -= n;
+  //   index += n;
+  // }
+
+  // if (len > 0) {
+  //   smooth(&input[index], smoothed, len);
+  //   PaUtil_WriteRingBuffer(self->ringbuffer, smoothed, len);
+  // }
+
+  // highpass(input, input, length);
   PaUtil_WriteRingBuffer(self->ringbuffer, input, length);
 
 }
 
 
 #undef RB_LENGTH
+
