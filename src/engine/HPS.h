d9bd3c215b756d494d6c198bfbafc234edd3e505
Davorin Šego <davorin.sego@gmail.com>
2013-12-26 21:17:15 +0100

    hps outline

---
+++ src/engine/HPS.h
@@ -0,0 +1,15 @@
+/*
+    Copyright (c) 2013 Davorin Šego. All rights reserved.
+*/
+
+#include <complex.h>
+#include "Vec.h"
+#include "ffts.h"
+
+
+// Harmonic product spectrum
+typedef struct {
+
+  int a; // dummy
+
+} HPS;
