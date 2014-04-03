c79542e8531ca20b29f198ec44714b75bed2d401
Davorin Šego <davorin.sego@gmail.com>
2014-04-03 16:15:53 +0200

    fix memory leak

--- src/mac/ContentView.m
+++ src/mac/ContentView.m
@@ -5,27 +5,26 @@
 
 @implementation ContentView {
 
-  NSColor *bgColor1;
-  NSColor *bgColor2;
-  NSGradient *gradient;
+  NSColor *startColor;
+  NSColor *endColor;
 
 }
 
-
 - (void)awakeFromNib {
 
-  bgColor1 = [NSColor colorWithCalibratedRed:27.0/255.0 green:27.0/255.0 blue:27.0/255.0 alpha:1.0];
-  bgColor2 = [NSColor colorWithCalibratedRed:21.0/255.0 green:21.0/255.0 blue:21.0/255.0 alpha:1.0];
-  gradient = [NSGradient alloc];
+  startColor = [NSColor colorWithCalibratedRed:21.0/255.0 green:21.0/255.0 blue:21.0/255.0 alpha:1.0];
+  endColor =[NSColor colorWithCalibratedRed:27.0/255.0 green:27.0/255.0 blue:27.0/255.0 alpha:1.0];
 
 }
 
 
 - (void)drawRect:(NSRect)dirtyRect {
 
-  NSRect frame = [self frame];
-  gradient = [gradient initWithColorsAndLocations: bgColor2, 0.0, bgColor1, 100.0 / frame.size.height, nil];
-  [gradient drawInRect:frame angle:90];
+  NSGradient *gradient = [[NSGradient alloc] initWithColorsAndLocations:
+    startColor, 0.0,
+    endColor, 100.0 / self.frame.size.height, nil];
+
+  [gradient drawInRect:self.frame angle:90];
 
 }
 
