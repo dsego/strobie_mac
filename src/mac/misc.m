3ff294a17a4f8638d4b9eb7d72de13b2e6e89930
Davorin Šego <davorin.sego@gmail.com>
2014-06-12 00:20:13 +0200

    rephrase audio error dialog

--- src/mac/misc.m
+++ src/mac/misc.m
@@ -8,10 +8,8 @@
 void audioDeviceErrorAlert(void) {
 
   NSAlert *alert = [[NSAlert alloc] init];
-  alert.messageText = @"Audio device error";
-  alert.informativeText = @"Current audio settings aren't working. "
-                          "Please try a different sample rate and buffer size, "
-                          "or choose a different audio device.";
+  alert.messageText = @"No audio input";
+  alert.informativeText = @"Try changing your audio settings or choose a different device.";
   [alert runModal];
 
 }
