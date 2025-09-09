1423ec555a542d7cc4024cf580a7bdc5ef3c9b61
Davorin Šego <davorin.sego@gmail.com>
2025-09-09 12:53:13 +0200

    Chk

---
+++ src/mac/HelpWindow.m
@@ -0,0 +1,14 @@
+// Copyright (c) Davorin Šego. All rights reserved.
+
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface HelpView : NSImageView
+@end
+
+@implementation HelpView
+
+- (BOOL)mouseDownCanMoveWindow {return YES; }
+
+@end
